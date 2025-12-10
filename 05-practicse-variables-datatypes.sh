5c33f4a62db6501d316ccd72cde7dd86a52ed139
Bhaskar <bhaskardevops@gmail.com>
2025-12-10 19:37:52 +0530

    changes

--- 05-practicse-variables-datatypes.sh
+++ 05-practicse-variables-datatypes.sh
@@ -3,10 +3,15 @@
 echo "Hello World"
 
 # Arguments
-PERSON1=$1
-PERSON2=$2
+PERSON1="Modi"
+PERSON2="Putin"
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: What are you learning recently?"
 echo "$PERSON2:: I am learning Shell scripting $PERSON1"
+
+echo "Please enter your PIN number"
+read -s PIN_NUMBER # here PIN_NUMBER is variable
+
+echo "PIN entered is: $PIN_NUMBER"
