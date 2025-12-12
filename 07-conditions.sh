f04fe3ed2234996295f102f9eb9b3187e23c43b7
Bhaskar <bhaskardevops@gmail.com>
2025-12-12 11:01:09 +0530

    changes

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,7 +3,7 @@
 echo "Enter a number to check EVEN or ODD"
 read $NUM
 
-if [ $(($NUM %2)) -eq 0 ]; then
+if [ $(($NUM % 2)) -eq 0 ]; then
     echo "given num $NUM is EVEN"
 else
     echo "given num $NUM is ODD"
